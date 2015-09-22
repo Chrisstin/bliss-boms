31daa0b53d1e91e5d657cb430b414d780b30d031
Christine Newbery <chris@Christines-iMac.local>
2015-09-22 19:47:27 +1000

    Initial Commit

---
+++ Bliss Boms/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Bliss Boms
+//
+//  Created by Christine Newbery on 22/09/2015.
+//  Copyright (c) 2015 Christine Newbery. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
